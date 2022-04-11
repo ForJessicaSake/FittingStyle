8b7dd455b4fed4e4b8f45da2db16bfad95fcf6ff
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-11 02:07:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //DOM MANIPULATION
 var button = document.getElementById('btn').addEventListener('click', function() {
-    document.getElementById('btn').style.backgroundColor = 'yellow';
+    document.getElementById('btn').style.backgroundColor = 'red';
 });
