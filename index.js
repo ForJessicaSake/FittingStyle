d02709480f37c8411da2ebe8238b7b4d04f2578a
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-11 13:57:03 +0100

    work in progress

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-//DOM MANIPULATION
-var button = document.getElementById('btn').addEventListener('click', function() {
-    document.getElementById('btn').style.backgroundColor = 'yellow';
-});
+const buttonClickEffect = document.querySelectorAll(".btn");
+for (let i = 0; i < buttonClickEffect.length; i++) {
+    buttonClickEffect[i].addEventListener("click", function() {
+        buttonClickEffect[i].style.backgroundColor = "red";
+    });
+}
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block"
+}
+
+span.onclick = function() {
+    modal.style.display = "none"
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
+var shopmodal = document.getElementById("shopModal");
+var shopbtn = document.getElementById("shopBtn");
+var shopspan = document.getElementsByClassName("shopClose")[0];
+shopbtn.onclick = function() {
+    shopmodal.style.display = "block"
+}
+
+shopspan.onclick = function() {
+    shopmodal.style.display = "none"
+}
+
+window.onclick = function(event) {
+    if (event.target == shopmodal) {
+        shopmodal.style.display = "none"
+    }
+}
