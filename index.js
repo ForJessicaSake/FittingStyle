4ade94334858a19428d9fb9049fb825003635b5e
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-11 14:09:43 +0100

    work in progress

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
-    modal.style.display = "block"
+    modal.style.display = "block";
 }
 
 span.onclick = function() {
@@ -18,22 +18,6 @@ span.onclick = function() {
 
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none"
-    }
-}
-var shopmodal = document.getElementById("shopModal");
-var shopbtn = document.getElementById("shopBtn");
-var shopspan = document.getElementsByClassName("shopClose")[0];
-shopbtn.onclick = function() {
-    shopmodal.style.display = "block"
-}
-
-shopspan.onclick = function() {
-    shopmodal.style.display = "none"
-}
-
-window.onclick = function(event) {
-    if (event.target == shopmodal) {
-        shopmodal.style.display = "none"
+        modal.style.display = "none";
     }
 }
